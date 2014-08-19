efa1483c340a3ee35aa4899506d5d206aa9281a7
spudgy <antoniohermano@gmail.com>
2014-08-19 15:27:39 +0200

    Revert "up"
    This reverts commit 278b71d156b21e092b8c03acd2f245a797b28070.

--- BinWars/Scripts/Heroes/Caitlyn.lua
+++ BinWars/Scripts/Heroes/Caitlyn.lua
@@ -1,59 +1,10 @@
-function SpawnTrap(trap)
-	print('spawned!')
-		--[   250] trap:AnimateSpell(5,'Spell1',1.0,1.0) -- B0, animate trap
-		--[     0] ParticleEmiter() --count: 2 - 87 ...
-		--[    16] trap:ApplyBuff(0,2,'CaitlynYordleTrap',240.000000) --Stacks: 1, Visible: 0
-		--[     0] pe:AddParticle(trap,'caitlyn_Base_yordleTrap_idle_green.troy') --Applied to Cupcake Trap
-		--[     0] pe:AddParticle(trap,'caitlyn_Base_yordleTrap_set.troy') --Applied to Cupcake Trap
-end
-function NidaleeQHit(projectile,target)
-        if target then
-        target:TakeDamage(30,projectile.owner)
-        --apply Q to target
-        --projectile:Hit(target) --HitEffectName
-        end
-end
-
 function CastSpell(self,spellIndex,x,y)
 	if spellIndex == 0 then 
-
-                        spell = CSpellWork(self,spellIndex,x,y) --CaitlynPiltoverPeacemaker
-                        
-                        spell.cooldown = 0
-                        spell.manaCost = 0
-                        --calc position with cast range = 1500
-                        spell.projectile = CreateProjectile(self,x,y,1300,40,NidaleeQHit)
-
-                        self:QueueSpell(spell)
+		SendMissile(self,'CaitlynPiltoverPeacemaker',x,y,0x110);
 	elseif spellIndex == 1 then 
-        spell = CSpellWork(self,spellIndex,x,y) --CaitlynYordleTrap
-        spell.iUpdatePos = 1  --[    62] self:SetupSpell() --[40000019] 1 , Updates Position
-        spell.fVisible = 1.0
-        spell.cooldown = 0
-        spell.mana = 0
-
-		spell:CreateObject(self,124,x,y,'Cupcake Trap','CaitlynTrap',100,SpawnTrap)
-
-        self:QueueSpell(spell)
+		SendMissile(self,'CaitlynYordleTrap',x,y,0x110);
 	elseif spellIndex == 2 then 
-                        spell = CSpellWork(self,spellIndex,x,y) --CaitlynEntrapment
-                        
-                        spell.cooldown = 0
-                        spell.manaCost = 0
-
-                        spell:SetDash(x,y,1000.0)
-
-                        self:QueueSpell(spell)
---[[		[0 ] } OnCastSpell(130,1949.763870,715.183163) {
-[    47] self:SetupSpell() --[40000019] 1 , Updates Position
-[     0] self:CastSpell(spellIndex,x,y) --[0B2C7F54] NOT FOUND / 0066 flag  [0B2C7F54] CaitlynEntrapment
-[   171] self:SetPosition(...) --50 sets face position
-[     0] self:CastSpell(spellIndex,x,y) --[02CD6B05] NOT FOUND / 0066 flag  [02CD6B05] CaitlynEntrapmentMissile
---6E
---6B
-[    63] self:DoDash(...) --64]]
-
-		--SendMissile(self,'CaitlynEntrapment',x,y,0x110);
+		SendMissile(self,'CaitlynEntrapment',x,y,0x110);
 		--SendSpell(self,'Spell3',5,0)
 	end
 end
--- BinWars/Scripts/Heroes/Leesin.lua
+++ BinWars/Scripts/Heroes/Leesin.lua
@@ -1,45 +1,15 @@
-function LeesinQHit(projectile,target)
-        if target then
-        print(target.model..' - LEESIN Q HIT !')
-        --apply Q to target
-        --76 ??
-        --23 vision spawn
-        --23 vision spawn
-        --65 load hero?
-        projectile:Destroy() --5A NETID, destroy missile?
-        --projectile.owner:SetTemporarySpell(0,'BlindMonkQTwo')--17 Setup spellname, 2nd leesin skill (BlindMonkQTwo)
-        projectile:Hit(target) --26 NETID ..
-        --87 SpawnParticle
-        --B7 Apply buff to target
-        CreateParticle(projectile.owner,target,'blindMonk_Q_resonatingStrike_tar.troy',0) --splash
-        CreateParticle(projectile.owner,target,'blindMonk_Q_tar_indicator.troy',0) --floor/head indicator, must expire
-        --CreateParticle(projectile.owner,target,'blindMonk_Q_resonatingStrike_tar_blood.troy',0) --?
-        --CreateParticle(projectile.owner,target,'leesin_skin05_Q_tar_sound.troy',0)
-        --target:ApplyBuff(3,'BlindMonkQOne',3.0,1)
-        end
-end
-
-
 function CastSpell(self,spellIndex,x,y)
 	if spellIndex == 0 then 
 		--self:CastSpell(spellIndex,x,y);
 		--6D?
 		spell = CSpellWork(self,_Q,x,y) 
-        spell.manaCost = 0
-        spell.cooldown = 0
         spell.updatePos = 0
         spell.stop = true
         spell.releaseCast = true
-        spell.projectile = CreateProjectile(self,x,y,1800,60,LeesinQHit)
         spell:AddBuff(2,'blindmonkpassive_cosmetic',3.0,2) --[   172] self:ApplyBuff(2,'blindmonkpassive_cosmetic',3.000000) --Stacks: 2, Visible: 0
                 
                 self:QueueSpell(spell)
                 --[  1984] self:SetSpellName(1,'BlindMonkWOne') ? reset if did not hit
-                --calc position with cast range = 1100
-                --Hero collision only
-                --LineWidth       "60.0000"
-                --CastRange     "1100.0000"
-                --MissileSpeed  "1800.0000"
 	elseif spellIndex == 1 then 
 		spell = CSpellWork(self,_W,x,y) 
 		spell:AddBuff(2,'blindmonkwoneshield',4.0,1) --4 seconds, 1 stack
@@ -50,8 +20,6 @@ function CastSpell(self,spellIndex,x,y)
                 
 	elseif spellIndex == 2 then 
 		spell = CSpellWork(self,_E,x,y) --EzrealMysticShot
-        spell.manaCost = 0
-        spell.cooldown = 0
         spell.stop = true--[    15] MoveAns(self,...) --[1] STOP
                 
                 p1 = spell:AddParticle(self,self,'blindMonk_thunderCrash_impact_02.troy',0.25)
